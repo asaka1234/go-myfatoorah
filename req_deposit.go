67e86a2c7477e73c610f92e4d7f3a8f7c77f14b3
cyrildou <cyrildou@live.cn>
2025-07-19 05:18:11 +0800

    add log

--- req_deposit.go
+++ req_deposit.go
@@ -2,6 +2,9 @@ package go_myfatoorah
 
 import (
 	"crypto/tls"
+	"fmt"
+	"github.com/asaka1234/go-myfatoorah/utils"
+	jsoniter "github.com/json-iterator/go"
 	"github.com/mitchellh/mapstructure"
 )
 
@@ -50,12 +53,21 @@ func (cli *Client) Deposit(req MyFatoorahDepositReq) (*MyFatoorahDepositRsp, err
 		SetError(&result).
 		Post(rawURL)
 
-	cli.logger.Infof("go_myfatoorah==>url:%s, err:%+v, body:%s", rawURL, err, resp.Body())
-
-	//fmt.Printf("result: %s\n", string(resp.Body()))
+	restLog, _ := jsoniter.ConfigCompatibleWithStandardLibrary.Marshal(utils.GetRestyLog(resp))
+	cli.logger.Infof("PSPResty#myfatoorah#deposit->%+v", string(restLog))
 
 	if err != nil {
 		return nil, err
+	}
+
+	if resp.StatusCode() != 200 {
+		//反序列化错误会在此捕捉
+		return nil, fmt.Errorf("status code: %d", resp.StatusCode())
+	}
+
+	if resp.Error() != nil {
+		//反序列化错误会在此捕捉
+		return nil, fmt.Errorf("%v, body:%s", resp.Error(), resp.Body())
 	}
 
 	return &result, nil
